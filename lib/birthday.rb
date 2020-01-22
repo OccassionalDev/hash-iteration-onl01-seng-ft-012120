cbffbc468e4f59604a948b66acf0e46a47b0732e
Erica Schimmel <ericaschimmel01@gmail.com>
2020-01-22 05:58:34 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,7 +13,10 @@ def happy_birthday(birthday_kids)
 end
 
 def age_appropriate_birthday
-  
+  #Step 1: For each kids' name, iterate through searching for ages 12 and younger
+  birthday_kids.each do |kids_name, age|
+    #Step 2: If age < 12, output
+  end
   
 end
 
